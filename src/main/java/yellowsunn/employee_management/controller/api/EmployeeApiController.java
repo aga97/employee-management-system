19ee5c0a62839787c7947f6d275bcab1036db1c7
aga97 <aga97@naver.com>
2020-10-22 14:04:43 +0900

    Update EmployeeApiController.java
    fix

--- src/main/java/yellowsunn/employee_management/controller/api/EmployeeApiController.java
+++ src/main/java/yellowsunn/employee_management/controller/api/EmployeeApiController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import yellowsunn.employee_management.dto.CurEmpInfoDto;
 import yellowsunn.employee_management.dto.condition.EmpSearchCondition;
 import yellowsunn.employee_management.service.EmpInfoService;
